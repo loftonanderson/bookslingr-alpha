0d23e565632cf16ceb1feb76c8c6246672a8f38b
loftonanderson <lja5750@uncw.edu>
2016-04-01 13:28:37 -0400

    Initial Commit

---
+++ bookslingr-alpha/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bookslingr-alpha
+//
+//  Created by Lofton Anderson on 4/1/16.
+//  Copyright © 2016 Lofton Anderson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
